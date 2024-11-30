6f99f63b9551c9b0af12fbf4a6ff5436a8055178
Abdelrahman Bahig <23p0390@eng.asu.edu.eg>
2024-11-30 22:04:35 +0200

    mmmm

--- src/entities/Cart.java
+++ src/entities/Cart.java
@@ -1,4 +1,22 @@
 package src.entities;
 
 public class Cart {
+    private int id;
+    private Customer customer;
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 }
